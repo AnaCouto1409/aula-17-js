04188a7931376b1e938ba653c60b48376f153950
AnaCouto1409 <35375044+AnaCouto1409@users.noreply.github.com>
2021-09-21 19:57:52 -0300

    Add files via upload

---
+++ aula17-2.js
@@ -0,0 +1,49 @@
+//map, reduce
+
+let numeros = [1,2,3,4,5,6];
+let numerosDobro = numeros.map(function(elemento,indice){
+    return `${elemento}`;
+});
+console.log(numerosDobro);
+//map é parecido com forEach e que o map retorna um array e o forEach
+let table ={
+<table>
+    <tr>
+        <th>Numero</th>
+        <th>Saldo</th>
+        <th>Titular</th>
+    </tr>
+    let table={
+ </table>
+<tr>
+    <td> R${elemento.numero}</td>
+    <td> R${elemento.saldo}</td>
+    <td> R${elemento.titular}</td>
+</tr>
+}
+}
+//python é mais voltado ao back END TODAS AS PLAICAÇÃOES QUE EXISTEM NO PYTHOM
+//reduce- boramais
+table= `
+</table>
+<tr>
+    <td> R${elemento.numero}</td>
+    <td> R${elemento.saldo}</td>
+    <td> R${elemento.titular}</td>
+</tr>;
+for (let cliente of meuBanco.banco.clientes){
+    table + = `
+    //<tr>
+   // <td> R${elemento.numero}</td>
+    <//td> R${elemento.saldo}</td>
+   // <td> R${elemento.titular}</td>
+//</tr>;
+//}
+//reduce
+let soma2 = 0;
+for(let i = 0; i<numeros.length;i++){
+    soma += numeros[i];
+}
+soma.numerosDobro.reduce(function(acumulator,elemento,indice){
+return acumulator + '-' + elemento;
+});
